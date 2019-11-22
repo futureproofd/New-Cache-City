4c22fa72514b79129a814eaadf6a6b4dc74718c2
Marcus Plienegger <futureproofd@users.noreply.github.com>
2019-11-21 21:54:08 -0500

    Static Map on CacheDetail
    Added static map to cache detail with zoom dialog

--- frontend/components/CacheDetail.js
+++ frontend/components/CacheDetail.js
@@ -1,13 +1,30 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/destructuring-assignment */
-import React from 'react';
-import { FormStyle } from '../styles/FormStyle';
+import React, { useState } from 'react';
+import {
+  FormEditStyle,
+  DialogImage,
+  MapImage,
+  CacheImage,
+} from '../styles/FormStyle';
 
 const CacheDetail = (props) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!isOpen);
+  };
+
   const { item, user } = props.location.state;
   return (
-    <FormStyle key={item.id}>
+    <FormEditStyle key={item.id}>
       <fieldset disabled={!user} aria-busy={false}>
         <label htmlFor="name">
+          Name
           <input
             name="name"
             type="text"
@@ -16,15 +33,56 @@ const CacheDetail = (props) => {
             value={item.name}
           />
         </label>
-        <img src={item.photo} alt={item.name} />
-        <p>{item.description}</p>
-        <h1>{` lat: ${item.location.coordinates[1]}, lng: ${item.location.coordinates[0]}`}</h1>
-        <img
-          alt="location"
-          src={`https://maps.googleapis.com/maps/api/staticmap?center=${item.location.coordinates[1]},${item.location.coordinates[0]}&zoom=14&size=800x150&key=${process.env.GOOGLE_API_KEY}&markers=${item.location.coordinates[1]},${item.location.coordinates[0]}&scale=2`}
+        <CacheImage src={item.photo} alt={item.name} />
+        <label htmlFor="description">
+          Description
+          <input
+            name="description"
+            type="text"
+            required
+            onChange={() => console.log('user edit')}
+            value={item.description}
+          />
+        </label>
+        <label htmlFor="coordinates">
+          Coordinates
+          <input
+            name="coordinates"
+            type="text"
+            required
+            onChange={() => console.log('user edit')}
+            value={` lat: ${item.location.coordinates[1]}, lng: ${item.location.coordinates[0]}`}
+          />
+        </label>
+
+        <label htmlFor="address">
+          Approximate Location
+          <input
+            name="address"
+            type="text"
+            required
+            onChange={() => console.log('user edit')}
+            value={item.location.address}
+          />
+        </label>
+        <MapImage
+          name="staticMapPrev"
+          alt={item.location.address}
+          onClick={handleOpen}
+          src={`https://maps.googleapis.com/maps/api/staticmap?center=${item.location.coordinates[1]},${item.location.coordinates[0]}&zoom=17&size=700x200&key=${process.env.GOOGLE_API_KEY}&markers=${item.location.coordinates[1]},${item.location.coordinates[0]}&scale=2`}
         />
+        {isOpen && (
+          <dialog open style={{ position: 'absolute' }} onClick={handleOpen}>
+            <DialogImage
+              name="staticMap"
+              alt={item.location.address}
+              onClick={handleOpen}
+              src={`https://maps.googleapis.com/maps/api/staticmap?center=${item.location.coordinates[1]},${item.location.coordinates[0]}&zoom=19&size=1000x500&key=${process.env.GOOGLE_API_KEY}&markers=${item.location.coordinates[1]},${item.location.coordinates[0]}&scale=2`}
+            />
+          </dialog>
+        )}
       </fieldset>
-    </FormStyle>
+    </FormEditStyle>
   );
 };
 
--- frontend/styles/FormStyle.js
+++ frontend/styles/FormStyle.js
@@ -83,6 +83,100 @@ export const FormStyle = styled.form`
   }
 `;
 
+export const FormEditStyle = styled.form`
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+  background: rgba(0, 0, 0, 0.02);
+  border: 5px solid white;
+  padding: 20px;
+  font-size: 1.2rem;
+  line-height: 1.5;
+  font-weight: 400;
+  label {
+    display: block;
+    margin-bottom: 1rem;
+  }
+  dialog {
+    box-shadow: 0 4px 2px -2px grey;
+    position: static;
+    width: 950px;
+    height: 650px;
+    top: 50%;
+  }
+  h1 {
+    font-weight: 100;
+    font-size: 0.75rem;
+    padding: 0.5rem;
+  }
+
+  input,
+  textarea,
+  select {
+    width: 60%;
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: none;
+    background: rgba(0, 0, 0, 0.02);
+    &:focus {
+      outline: 0;
+      border-color: ${props => props.theme.blue};
+    }
+  }
+  button,
+  input[type="submit"] {
+    width: auto;
+    background: ${props => props.theme.blue};
+    color: white;
+    border: 0;
+    font-size: 2rem;
+    font-weight: 600;
+    padding: 0.5rem 1.2rem;
+    cursor: pointer;
+    &[disabled] {
+      opacity: 0.8;
+      cursor: not-allowed;
+    }
+  }
+  fieldset {
+    border: 0;
+    padding: 0;
+
+    &[disabled] {
+      opacity: 1;
+    }
+    &::before {
+      height: 10px;
+      content: "";
+      display: block;
+      background-image: linear-gradient(
+        to right,
+        #0061ff 0%,
+        #ffffff 50%,
+        #0061ff 100%
+      );
+    }
+    &[aria-busy="true"]::before {
+      background-size: 50% auto;
+      animation: ${loading} 0.5s linear infinite;
+    }
+  }
+`;
+
+export const DialogImage = styled.img`
+  width: inherit;
+  height: inherit;
+`;
+
+export const MapImage = styled.img`
+  width: 80%;
+  height: 80%;
+  padding: 0.5rem;
+`;
+
+export const CacheImage = styled.img`
+  width: 600px;
+  padding: 0.5rem;
+`;
+
 export const Message = styled.label`
   margin-bottom: 0.2em;
   color: red;
